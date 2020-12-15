0cb8bae69030c10a5e2df76cfffeb67495bcc838
Benoit <benoit.beaucourt@laplateforme.io>
2020-12-15 15:14:27 +0100

    Data exploration

--- new-car.py
+++ new-car.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pandas as pd
+import matplotlib as plt 
 
- def builddf(self):
-        df = pd.read_csv('Flowers.csv')
-        subset = df[['x', 'y']]
-        flowers = [tuple(x) for x in subset.to_numpy()]
-        f = flowers
-        genome = deepcopy(f)
-        np.random.shuffle(genome)
-        return genome
+class Car_data:
+    
+    def __init__(self):
+        self.df = self.builddf()
+    
+    def builddf(self):
+        df = pd.read_csv('carData.csv')
+        subset = df [['Car_Name', 'Year', 'Selling_Price', 'Present_Price', 'Kms_Driven', 'Fuel_Type', 'Seller_Type', 'Transmission', 'Owner']]
+        car = [tuple(x) for x in subset.to_numpy()]
+        return df
+
+car_data = Car_data()    
+print("data frame", car_data.df)
+print('mean', car_data.df.mean())
+print('describe', car_data.df.describe())
