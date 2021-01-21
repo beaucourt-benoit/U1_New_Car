5dc7897db1d52ef2fb10c297ab4d002459eade79
Benoit <benoit.beaucourt@laplateforme.io>
2021-01-21 09:19:52 +0100

    New_car in notebook

--- new-car.py
+++ new-car.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 class Car_data:
     
@@ -14,11 +16,35 @@ class Car_data:
         return df
 
 car_data = Car_data()    
-print("data frame", car_data.df)
-print('mean', car_data.df.mean())
-print('describe', car_data.df.describe())
+print("\n data frame \n", car_data.df)
+print('\n mean \n', car_data.df.mean())
+print('\n describe \n', car_data.df.describe())
 
-plt.hist(car_data.df, bins = 5, color = ('yellow', 'black', 'white', 'blue', 'red', 'green', 'purple', 'orange', 'brown'), edgecolor = 'red')
+#plt.hist(car_data.df.Year, bins = 20, color = ('yellow'), edgecolor = 'red')
+#plt.xlabel('valeurs')
+#plt.ylabel('nombres')
+#plt.title('Year ')
+
+#plt.hist(car_data.df.Selling_Price, bins = 5, color = ('black'), edgecolor = 'red')
+#plt.xlabel('valeurs')
+#plt.ylabel('nombres')
+#plt.title('Selling price ')
+
+#plt.hist(car_data.df.Kms_Driven, bins = 100, color = ('black'), edgecolor = 'red')
+#plt.xlabel('valeurs')
+#plt.ylabel('nombres')
+#plt.title('kilometers ')
+
+plt.hist(car_data.df.Present_Price, bins = 50, color = ('black'), edgecolor = 'red')
 plt.xlabel('valeurs')
 plt.ylabel('nombres')
-plt.title('Exemple d\' histogramme simple')
+plt.title('Present price "Argus" ')
+
+
+#sns.set_theme(style="ticks")
+#exercise = sns.load_dataset("car_data.df")
+#g = sns.catplot(x="Year", y="Selling_Price", hue="Kms_Driven", data=car_data.df)
+
+a= np.random.randn(100)
+b=np.random.randn(100)
+sns.jointplot(a,b)
